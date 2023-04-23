3df9d0f37cf5be9a25420caffc7db34567dad9b7
Huck Boles <huck@huck.website>
2023-04-23 08:03:16 -0500

    lsp popup borders

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,6 +32,25 @@ return {
                     lspconfig[server_name].setup({})
                 end
             })
+
+            local border = {
+                {"┌", "FloatBorder"},
+                {"─", "FloatBorder"},
+                {"┐", "FloatBorder"},
+                {"│", "FloatBorder"},
+                {"┘", "FloatBorder"},
+                {"─", "FloatBorder"},
+                {"└", "FloatBorder"},
+                {"│", "FloatBorder"},
+            }
+
+            -- To instead override globally
+            local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+            function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+                opts = opts or {}
+                opts.border = opts.border or border
+                return orig_util_open_floating_preview(contents, syntax, opts, ...)
+            end
         end
     },
 }
